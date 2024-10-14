0e99c08ee39fc153711d53988f528d6ca64d3ada
Jan Sevcik <jan.sevcik@student.gyarab.cz>
2024-10-14 08:53:14 +0200

    Pecenej varenej

--- src/Hodina5/Main.java
+++ src/Hodina5/Main.java
@@ -1,12 +1,30 @@
 package Hodina5;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Tovarna tovarna = new Tovarna();
-        Jidlo j = tovarna.vyrobJidlo("spekacek");
 
-        System.out.println(j.dejCenu());
+        ArrayList<Jidlo> arr = new ArrayList<>();
+
+        while(true){
+            String slovo = sc.nextLine();
+            if (slovo.equals("done")) {
+                break;
+            }
+            arr.add(tovarna.vyrobJidlo(slovo));
+        }
+
+        int cena = 0;
+        for (Jidlo j : arr){
+            cena += j.dejCenu();
+            System.out.println(j.getClass().getSimpleName());
+        }
+        System.out.println("cena: " + cena);
     }
 }
 
